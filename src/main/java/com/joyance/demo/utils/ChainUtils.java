9879019935a5085d9c6e26fa6008400261059ffb
关悦 <freedomguanyue@sohu.com>
2019-04-16 12:59:34 +0800

    新增demo

---
+++ src/main/java/com/joyance/demo/utils/ChainUtils.java
@@ -0,0 +1,28 @@
+package com.joyance.demo.utils;
+
+public class ChainUtils {
+
+	private String defaultTag() {
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        for (StackTraceElement e : stackTrace) {
+        	if(e.getClassName().startsWith("com.joyance")){
+        		System.out.println(e.getClassName() + "\t"
+                        + e.getMethodName() + "\t" + e.getLineNumber());
+        	}
+        }
+        StackTraceElement log = stackTrace[1];
+        String tag = null;
+        for (int i = 1; i < stackTrace.length; i++) {
+            StackTraceElement e = stackTrace[i];
+            if (!e.getClassName().equals(log.getClassName())) {
+                tag = e.getClassName() + "." + e.getMethodName();
+                break;
+            }
+        }
+        if (tag == null) {
+            tag = log.getClassName() + "." + log.getMethodName();
+        }
+        System.out.println(tag);
+        return tag;
+    }
+}
