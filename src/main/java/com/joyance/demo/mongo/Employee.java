75e8d0f7f296344cdddc7468cc2104da567cfba6
joyanceguan <freedomguanyue@sohu.com>
2019-06-23 23:33:56 +0800

    新增测试方法

--- src/main/java/com/joyance/demo/mongo/Employee.java
+++ src/main/java/com/joyance/demo/mongo/Employee.java
@@ -31,6 +31,30 @@ import org.springframework.data.mongodb.core.mapping.Field;
           this.name = name;
           this.age = age;
       }
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
   
-      // getter & setter
+      
   }
--- src/test/java/com/joyance/demo/mongo/Base.java
+++ src/test/java/com/joyance/demo/mongo/Base.java
@@ -1,11 +1,14 @@
 package com.joyance.demo.mongo;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.alibaba.fastjson.JSON;
 import com.joyance.demo.mongo.dao.EmployeeDao;
 
 @RunWith(SpringJUnit4ClassRunner.class)  
@@ -17,8 +20,25 @@ public class Base {
 	
 	@Test
 	public void test_save(){
-		Employee employee = new Employee("joy",28);
+		Employee employee = new Employee("joyance",29);
 		employeeDao.insertOneEmployee(employee);
+	}
+	
+	@Test
+	public void test_find(){
+		List<Employee> list = employeeDao.findAll();	
+		System.out.println(JSON.toJSONString(list));
+	}
+	
+	@Test
+	public void test_findByName(){
+		Employee employee = employeeDao.findByName("joy");	
+		System.out.println(JSON.toJSONString(employee));
+	}
+	
+	@Test
+	public void test_deleteOneEmployee(){
+		employeeDao.deleteOneEmployee("5d0f997459e0a1ab2c26d14c");
 	}
 }
 
